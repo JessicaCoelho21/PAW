f3ba5f4de584ca819db95f3ef8923eda974fe5d2
JessicaCoelho21 <44201718+JessicaCoelho21@users.noreply.github.com>
2020-04-22 15:07:15 +0100

    Exercise 2 with an unknown error (JSON is not defined)

--- PAW_FP_05/Exercicio1/routes/historicReviews.js
+++ PAW_FP_05/Exercicio1/routes/historicReviews.js
@@ -1,31 +1,33 @@
 const express = require("express")
 const router = express.Router()
 const path = require('path')
-let fs = require("fs")
+const fs = require("fs")
 
-router.get("/", (req, res) => {
+router.get('/list', (req, res) => {
     const dbPath = path.resolve('db', 'reviews.json');
     const rev = JS0N.parse(fs.readFileSync(dbPath));
 
-    res.render("pages/reviewEmailBook", { list: data.reverse() });
+    res.render('pages/reviewEmailBook', { list: rev });
 });
 
 router.get('/:email/:book', (req, res) => {
-    const dbPath = path.resolve('db', 'reviews.json');
-    const rev = JS0N.parse(fs.readFileSync(dbPath));
+    const dbPath = path.resolve('db', 'reviews.json')
+    const info = fs.readFileSync(dbPath)
+    const rev = JS0N.parse(info)
 
-    let index;
-    const email = req.params.email;
-    const book = req.params.book;
+    let index
+    const email = req.params.email
+    const book = req.params.book
 
-    for (let i = 0; i < reviews.length; i++) {
-        if (reviews[i].email === email && reviews[i].book.replace(/\s/, '') === book.replace(/\s/, '')) {
-            index = i;
+    for (let i = 0; i < rev.length; i++) {
+        if (rev[i].email === email && rev[i].book.replace(/\s/, '') === book.replace(/\s/, '')) {
+            index = i
+
             break;
         }
     }
 
-    res.render('pages/details', { item: reviews[index] });
+    res.render('pages/details', { item: rev[index] })
 });
 
-module.exports = router;
+module.exports = router
--- PAW_FP_05/Exercicio1/routes/index.js
+++ PAW_FP_05/Exercicio1/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-let fs = require("fs")
+const fs = require("fs")
 const path = require('path')
 
 router.get("/", (req, res) => {
