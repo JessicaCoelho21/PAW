778e5ab0b53aa60ac94663d92bcf742721a6616c
JessicaCoelho21 <44201718+JessicaCoelho21@users.noreply.github.com>
2020-04-23 16:57:45 +0100

    Exercicio 1

---
+++ PAW_FP_06/Exercicio1/controllers/EmployeeController.js
@@ -0,0 +1,71 @@
+var mongoose = require("mongoose");
+var Employee = require("../models/Employee");
+
+var employeeController = {};
+
+employeeController.list = async function(req, res) {
+    try {
+        const employeesList = await Employee.find()
+        res.render('employees', { employees: employeesList })
+    } catch (e) {
+        res.render('employees', { employee: [] })
+    }
+};
+
+//Show employee by id
+employeeController.show = async function(req, res) {
+    console.log('ID', req.params.id)
+
+    const employeesList = await Employee.findById(req.params.id);
+    res.render('employees/show', { employees })
+};
+
+//Create employee
+employeeController.create = function(req, res) {
+    res.render("employees/create");
+};
+
+//Save new employee
+employeeController.save = async function(req, res) {
+    try {
+        console.log('ok')
+        res.send('ok');
+        const result = await Employee.create(req.body)
+            //
+        res.redirect(`/employees/show/${ result._id }`)
+    } catch (e) {
+        res.send('error')
+    }
+};
+
+
+//Edit an employee
+employeeController.edit = async function(req, res) {
+    const employee = await Employee.findById(req.params.id)
+
+    res.render('employees/edit', {
+        employee
+    })
+};
+
+//Update an employee
+employeeController.update = async function(req, res) {
+    const result = await Employee.findById(req.params.id)
+
+    res.render = await Employee.findByIdAndUpdate(req.params.id, req.body)
+    res.redirect(`/employees/show/${ req.params.id }`)
+};
+
+//Delete an employee
+employeeController.delete = function(req, res) {
+    Employee.remove({ _id: req.params.id }, function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Employee deleted!");
+            res.redirect("/employees");
+        }
+    });
+};
+
+module.exports = employeeController;
