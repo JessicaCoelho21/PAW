c1fe091a85351966fe84101003f18b35a2f0b059
JessicaCoelho21 <44201718+JessicaCoelho21@users.noreply.github.com>
2020-04-29 14:12:22 +0100

    Exercise 1 done

--- PAW_FP_06/Exercicio1/controllers/EmployeeController.js
+++ PAW_FP_06/Exercicio1/controllers/EmployeeController.js
@@ -3,6 +3,7 @@ var Employee = require("../models/Employee");
 
 var employeeController = {};
 
+//List employees
 employeeController.list = async function(req, res) {
     try {
         const employeesList = await Employee.find()
@@ -53,34 +54,26 @@ employeeController.update = async function(req, res) {
 };
 
 //Delete an employee
-employeeController.delete = function(req, res) {
-    Employee.remove({ _id: req.params.id }, function(err) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Employee deleted!");
-            res.redirect("/employees");
-        }
-    });
+employeeController.delete = async function(req, res) {
+    await Employee.findByIdAndDelete(req.params.id)
+    res.redirect('/employees')
 };
 
-employeeController.listPositions = async function(req, res) {
-    try {
-        const filters = {}
-        if (req.query.salary) {
-            filters.salary = {
-                $gte: parseFloat(req.query.salary)
-            }
-        }
-        if (req.query.position) {
-            filters.position = req.query.position
-        }
-        const employeesList = await Employee.find(filters)
-        res.render('employees', { employees: employeesList, filters: req.query })
-    } catch (e) {
-        res.render('employees', { employees: [] })
-    }
+//Search for an employee
+employeeController.search = async function(req, res) {
+    res.render('employees/search')
+};
 
-}
+//Search for salary
+employeeController.searchSalary = async function(req, res) {
+    const result = await Employee.find({ salary: { $gt: req.body.salary } });
+    res.render('employees', { employees: result })
+};
+
+//Search for a certain position in the enterprise
+employeeController.searchPosition = async function(req, res) {
+    const result = await Employee.find({ position: req.body.position });
+    res.render('employees', { employees: result })
+};
 
 module.exports = employeeController;
